b0892bfe9597e598b745f5f501255be7933681c6
Ohta Akira <waterlow3442@gmail.com>
2020-10-28 20:54:51 +0900

    履歴機能

--- src/board.tsx
+++ src/board.tsx
@@ -1,52 +1,16 @@
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import './board.css'
 import Square from './square'
 
-const calculateWinner = (squares: number[]) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  const line = lines.find(([a, b, c]) => (
-    squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
-  ));
-  return line && squares[line[0]]
-}
-
-const Board: React.FC = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setIsNext] = useState(true)
-  const renderSquare = useCallback((i) => {
-    const handleClick = (i: number) => {
-      if (calculateWinner(squares) || squares[i]) return
-
-      const newSquares = [...squares]
-      newSquares[i] = xIsNext ? 'X' : 'O'
-      setSquares(newSquares)
-      setIsNext(!xIsNext)
-    }
-
-    const value = squares[i]
-    return <Square value={value} onClick={() => handleClick(i)}/>
-  }, [squares, xIsNext])
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+const Board: React.FC<{ squares: number[], onClick: (i: number) => void }> = (props) => {
+  const renderSquare = (i: number) => (
+    <Square
+      value={props.squares[i]}
+      onClick={() => props.onClick(i)}/>
+  )
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/game.tsx
+++ src/game.tsx
@@ -1,16 +1,71 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import './game.css'
 import Board from './board'
 
+const calculateWinner = (squares: number[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  const line = lines.find(([a, b, c]) => (
+    squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+  ))
+  return line && squares[line[0]]
+}
+
 const Game: React.FC = () => {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }])
+  const [stepNumber, setStepNumber] = useState(0)
+  const xIsNext = (stepNumber % 2) === 0
+  const currentSquares = history[history.length - 1].squares
+  const winner = calculateWinner(currentSquares)
+
+  const status = useMemo(() => {
+    if (winner) {
+      return 'Winner: ' + winner
+    } else {
+      return 'Next player: ' + (xIsNext ? 'X' : 'O')
+    }
+  }, [winner, xIsNext])
+
+  const handleClick = (i: number) => {
+    if (winner || currentSquares[i]) return
+
+    const squares = currentSquares.slice()
+    squares[i] = xIsNext ? 'X' : 'O'
+    setStepNumber(stepNumber + 1)
+    setHistory(history.concat([{ squares }]))
+  }
+
+  const jumpTo = useCallback((step: number) => {
+    return () => {
+      setStepNumber(step)
+      setHistory(history.slice(0, step + 1))
+    }
+  }, [history])
+
+  const moves = history.map((_, move) =>
+    <li key={move}>
+      <button onClick={jumpTo(move)}>
+        {move === 0 ? 'Go to game start' : `Go to move #${move}` }
+      </button>
+    </li>
+  )
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={currentSquares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
